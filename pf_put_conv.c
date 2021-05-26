3d3c7ab8e23980958305802050c0c848bd8b1325
mejamoleo <reonarudo11919273@gmail.com>
2021-05-26 23:54:50 +0900

    :+1: create string specify conversion (#7)

--- pf_put_conv.c
+++ pf_put_conv.c
@@ -6,7 +6,7 @@
 /*   By: treo <treo@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 15:13:22 by treo              #+#    #+#             */
-/*   Updated: 2021/05/26 11:48:54 by treo             ###   ########.fr       */
+/*   Updated: 2021/05/26 14:55:18 by treo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 void	pf_put_conv(va_list args, t_conv_str *conv, int itr)
 {
-	char *str;
+	char	*str;
+	int		tmp;
 
 	if (conv->conv_char == 'i' || conv->conv_char == 'd')
 		str = ft_itoa(va_arg(args, int));
-
-
+	if (conv->conv_char == 's')
+		str = va_arg(args, char *);
 	ft_putstr_fd(str, 1);
 }
