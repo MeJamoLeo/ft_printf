8c83f69b8b54c9f4f2789496361762fee3638953
mejamoleo <reonarudo11919273@gmail.com>
2021-05-28 17:19:35 +0900

    temporary commit

--- pf_put_conv.c
+++ pf_put_conv.c
@@ -6,7 +6,7 @@
 /*   By: treo <treo@student.42tokyo.jp>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/25 15:13:22 by treo              #+#    #+#             */
-/*   Updated: 2021/05/28 12:45:18 by treo             ###   ########.fr       */
+/*   Updated: 2021/05/28 17:17:16 by treo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,24 @@
 
 void	pf_put_conv(va_list args, t_conv_str *conv)
 {
-	char	*str;
-	char	tmp;
+	if (conv->ast_field)
+		conv->field = va_arg(args, int);
+	if (conv->ast_precision)
+		conv->precision = va_arg(args, int);
 
-	str = NULL;
-	tmp = 0;
+	char	*str;
+	char	*tmp;
+	char	c;
+
 	if (conv->conv_char == 'i' || conv->conv_char == 'd')
 		str = ft_itoa(va_arg(args, int));
 	else if (conv->conv_char == 's')
-		str = va_arg(args, char *);
+		str = ft_strdup(va_arg(args, char *));
 	else if (conv->conv_char == 'c')
-		tmp = va_arg(args, int);
+	{
+		c = va_arg(args, int);
+		str = ft_strdup(&c);
+	}
 	else if (conv->conv_char == 'u')
 		str = ft_uitoa(va_arg(args, unsigned int));
 	else if (conv->conv_char == 'x')
@@ -33,11 +40,9 @@ void	pf_put_conv(va_list args, t_conv_str *conv)
 		str = ft_uitobase(va_arg(args, unsigned int), "0123456789ABCDEF");
 	else if (conv->conv_char == 'p')
 	{
-		ft_putstr_fd("0x", 1);
 		str = ft_ultobase((unsigned long)va_arg(args, void *), "0123456789abcdef");
+		tmp = str;
+		str = ft_strjoin("0x", str);
+		free(tmp);
 	}
-	if (conv->conv_char == 'c')
-		ft_putchar_fd(tmp, 1);
-	else
-		ft_putstr_fd(str, 1);
 }
